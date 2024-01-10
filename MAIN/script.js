b898ed2a4ea816d9c5bd0c59b23a81e11b8f3f1c
cgjedrem <65958280+cgjedrem@users.noreply.github.com>
2024-01-10 16:33:14 +0100

    modals

--- MAIN/script.js
+++ MAIN/script.js
@@ -1,7 +1,21 @@
 "use strict";
 
-const modal = document.querySelector(".modal");
+const modals = document.querySelectorAll(".modal");
 const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".close-modal");
-//list
+const btnsCloseModals = document.querySelectorAll(".close-modal");
 const btnsOpenModals = document.querySelectorAll(".show");
+
+//open modals
+for (let i = 0; i < btnsOpenModals.length; i++) {
+  btnsOpenModals[i].addEventListener("click", function () {
+    console.log(`button clicked`);
+    modals[i].classList.remove("hidden");
+    overlay.remove("hidden");
+
+    btnsCloseModals[i].addEventListener("click", function () {
+      console.log("close butten clicked");
+      modals[i].classList.add("hidden");
+      overlay.classList.add("hidden");
+    });
+  });
+}
